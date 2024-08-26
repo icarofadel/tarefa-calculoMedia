72e9f16928db50e60f4b389dc7092ef987dac697
RedSkull_7_7 <icaro.nata147@gmail.com>
2024-08-26 10:40:08 -0300

    fist commit

---
+++ src/br/com/icaro/MediaNota.java
@@ -0,0 +1,14 @@
+package br.com.icaro;
+
+public class MediaNota {
+	public static void main(String args[]) {
+		int nota1 = 10;
+		int nota2 = 5;
+		int nota3 = 2;
+		int nota4 = 1;
+		int soma = nota1+nota2+nota3+nota4;
+		int media = soma / 4;
+		
+		System.out.println(media);
+	}
+}
